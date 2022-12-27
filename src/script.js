3ed10facf6edfe9c584a7077a3dd7cdefe2185f6
Gilberto Junior <contato.gil.tulo@gmail.com>
2022-12-26 21:32:53 -0300

    Adicionado temporizador ano novo 2023

---
+++ src/script.js
@@ -0,0 +1,38 @@
+var dataAlvo = new Date("Jan 1, 2023 00:00:00").getTime();
+
+var contador = setInterval(function () {
+    conversorData();
+}, 1000);
+
+function conversorData() {
+    let hoje = new Date().getTime();
+    let distancia = dataAlvo - hoje;
+
+    if (distancia < 0) {
+        clearInterval(contador);
+        return document.getElementById("countdown").innerHTML = "Já acabou filhote, agora só em 2024"
+    }
+
+    let dias = Math.floor(distancia / (1000 * 60 * 60 * 24));
+    let diasFormatados = formatarValor(dias, dias.toString().length)
+
+    let horas = Math.floor((distancia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let horasFormatados = formatarValor(horas, horas.toString().length)
+
+    let minutos = Math.floor((distancia % (1000 * 60 * 60)) / (1000 * 60));
+    let minutosFormatados = formatarValor(minutos, minutos.toString().length)
+
+    let segundos = Math.floor((distancia % (1000 * 60)) / 1000);
+    let segundosFormatados = formatarValor(segundos, segundos.toString().length)
+
+    formatarData(diasFormatados, horasFormatados, minutosFormatados, segundosFormatados)
+};
+
+function formatarData(dias, horas, minutos, segundos) {
+    document.getElementById("countdown").innerHTML = `${dias} dias<br>${horas}:${minutos}:${segundos}`;
+};
+
+function formatarValor(num, targetLength) {
+    if (num < 10) targetLength = targetLength + 1;
+    return num.toString().padStart(targetLength, '0');
+};
